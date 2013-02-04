cdff417a042f3fbfa9a94ae4285e9caa544dddab
okparts <okparts@gmail.com>
2013-02-04 09:12:48 -0500

    Update HelloWorld2.c

--- HelloWorld2.c
+++ HelloWorld2.c
@@ -4,4 +4,5 @@ int main()
 {
     printf("Hello world again!\n");
     printf("I think I may wanna do something else here...\n");
+    printf("Welcome to the SENG 6230 Group Project GitHub Repository!\n");
 }
