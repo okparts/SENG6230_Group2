e0831752b8f7365a08f3e5086eba3d6dbdb936b4
Yuan He <heyuan89@gmail.com>
2013-05-01 00:22:18 -0400

    version 1.31

--- MTMS/test/MTMS/User_ControllerTest.java
+++ MTMS/test/MTMS/User_ControllerTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,18 +18,21 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Yuanhe1
+ * @author Yuan He
+ * 
+ * This the JUnit test set of the User controller module
  */
 public class User_ControllerTest {
     
     private User testUser;
     private String testPassword;
+
     
     public User_ControllerTest() {
-
+        
         testUser = new User();
         testUser.UserID = "testUserDoctor";
-        String testPassword = "test";
+        testPassword = "test";
         testUser.FirstName = "John";
         testUser.LastName = "Doe";
         testUser.UserType = U_Types.DOCTOR;
@@ -41,20 +45,18 @@ public class User_ControllerTest {
         Date testEnrollDay = new Date();
         
         try{
-             testUser.BirthDate = sdf.parse("1988-1-1");
-             testUser.EnrollDate = sdf.parse("2012-01-02");
+             testUser.BirthDate = sdf.parse("1988/1/1");
+             testUser.EnrollDate = sdf.parse("2012/01/02");
         }  catch(Exception e)
         {
             
         }
-        
-        User_Controller testConnection = new User_Controller();
-        boolean isAddUser = testConnection.CreateUser(testUser.UserID, testPassword, testUser.FirstName, testUser.LastName, testUser.BirthDate, testUser.EnrollDate, testUser.Address, testUser.Zipcode,testUser.PhoneNumber, testUser.UserType);
-    
+
     }
     
     @BeforeClass
     public static void setUpClass() {
+
     }
     
     @AfterClass
@@ -82,6 +84,25 @@ public class User_ControllerTest {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
+    
+    
+    
+        /**
+     * Test of CreateUser method, of class User_Controller.
+     */
+    @Test
+    public void testCreateUser() {
+        System.out.println("CreateUser");
+        boolean result;
+        User_Controller instance = new User_Controller();
+        boolean expResult = true;
+        result = instance.CreateUser(testUser.UserID, testPassword, testUser.FirstName, testUser.LastName, testUser.BirthDate, testUser.EnrollDate, testUser.Address, testUser.Zipcode,testUser.PhoneNumber, testUser.UserType);
+        System.out.print(instance.err);
+        assertEquals(expResult, result);
+       
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
 
     /**
      * Test of ValidateLogin method, of class User_Controller.
@@ -89,14 +110,15 @@ public class User_ControllerTest {
     @Test
     public void testValidateLogin() {
         System.out.println("ValidateLogin");
-        String UserID = "";
-        String PWD = "";
         User_Controller instance = new User_Controller();
-        User expResult = null;
-        User result = instance.ValidateLogin(UserID, PWD);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        User expResult = testUser;
+        User result = instance.ValidateLogin(testUser.UserID, testPassword);
+        Assert.assertEquals(expResult.UserID, result.UserID);
+        Assert.assertEquals(expResult.Address, result.Address);
+        Assert.assertEquals(expResult.BirthDate, result.BirthDate);
+        Assert.assertEquals(expResult.EnrollDate, result.EnrollDate);
+        Assert.assertEquals(expResult.FirstName, result.FirstName);
+        Assert.assertEquals(expResult.UserType, result.UserType);
     }
 
     /**
@@ -105,14 +127,11 @@ public class User_ControllerTest {
     @Test
     public void testSearchUser() {
         System.out.println("SearchUser");
-        String key = "";
-        String Type = "";
         User_Controller instance = new User_Controller();
-        ArrayList expResult = null;
-        ArrayList result = instance.SearchUser(key, Type);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        User expResult = testUser;
+        ArrayList result = instance.SearchUser(testUser.UserID, "ID");
+        assertEquals(expResult.UserID, ((User)result.get(0)).UserID);
+        assertEquals(expResult.UserType, ((User)result.get(0)).UserType);  
     }
 
     /**
@@ -121,15 +140,14 @@ public class User_ControllerTest {
     @Test
     public void testChangePassword_3args() {
         System.out.println("ChangePassword");
-        String UserID = "";
-        String OldPwd = "";
-        String NewPwd = "";
+        String UserID = testUser.UserID;
+        String OldPwd = testPassword;
+        String NewPwd = "newPassword";
         User_Controller instance = new User_Controller();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.ChangePassword(UserID, OldPwd, NewPwd);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult, result); 
+        assertEquals(testUser.PhoneNumber, instance.ValidateLogin(UserID, NewPwd).PhoneNumber);
     }
 
     /**
@@ -138,31 +156,13 @@ public class User_ControllerTest {
     @Test
     public void testChangePassword_String_String() {
         System.out.println("ChangePassword");
-        String UserID = "";
-        String NewPwd = "";
-        User_Controller instance = new User_Controller();
-        boolean expResult = false;
-        boolean result = instance.ChangePassword(UserID, NewPwd);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of CreateUser method, of class User_Controller.
-     */
-    @Test
-    public void testCreateUser() {
-        System.out.println("CreateUser");
-        boolean result = false;
+        String UserID = testUser.UserID;
+        String NewPwd = "adminPassword";
         User_Controller instance = new User_Controller();
         boolean expResult = true;
-        result = instance.CreateUser(testUser.UserID, testPassword, testUser.FirstName, testUser.LastName, testUser.BirthDate, testUser.EnrollDate, testUser.Address, testUser.Zipcode,testUser.PhoneNumber, testUser.UserType);
-        System.out.println(instance.err);
+        boolean result = instance.ChangePassword(UserID, NewPwd);
         assertEquals(expResult, result);
-       
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        assertEquals(testUser.PhoneNumber, instance.ValidateLogin(UserID, NewPwd).PhoneNumber);
     }
 
     /**
@@ -171,21 +171,16 @@ public class User_ControllerTest {
     @Test
     public void testUpdateUser_9args() {
         System.out.println("UpdateUser");
-        String UserID = "";
-        String First = "";
-        String Last = "";
-        Date Birth = null;
-        Date Enroll = null;
-        String Addr = "";
-        String Zip = "";
-        String Phone = "";
+        User temp = testUser;
+        temp.FirstName = "Yuan";
+        temp.LastName = "He";
+        temp.UserType = U_Types.LABTECH;
         U_Types UT = null;
         User_Controller instance = new User_Controller();
-        boolean expResult = false;
-        boolean result = instance.UpdateUser(UserID, First, Last, Birth, Enroll, Addr, Zip, Phone, UT);
+        boolean expResult = true;
+        boolean result = instance.UpdateUser(temp.UserID, temp.FirstName, temp.LastName, temp.BirthDate, temp.EnrollDate, temp.Address, temp.Zipcode, temp.PhoneNumber, temp.UserType);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(U_Types.LABTECH, instance.ValidateLogin(testUser.UserID, "adminPassword").UserType);
     }
 
     /**
@@ -194,33 +189,28 @@ public class User_ControllerTest {
     @Test
     public void testUpdateUser_7args() {
         System.out.println("UpdateUser");
-        String UserID = "";
-        String First = "";
-        String Last = "";
-        Date Birth = null;
-        String Addr = "";
-        String Zip = "";
-        String Phone = "";
+        User temp = testUser;
+        temp.FirstName = "John";
+        temp.LastName = "Doe";
+        temp.UserType = U_Types.LABTECH;
+        U_Types UT = null;
         User_Controller instance = new User_Controller();
-        boolean expResult = false;
-        boolean result = instance.UpdateUser(UserID, First, Last, Birth, Addr, Zip, Phone);
+        boolean expResult = true;
+        boolean result = instance.UpdateUser(temp.UserID, temp.FirstName, temp.LastName, temp.BirthDate, temp.Address, temp.Zipcode, temp.PhoneNumber);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of DeleteUser method, of class User_Controller.
      */
-    @Test
-    public void testDeleteUser() {
+
+   @Test
+   public void testDeleteUser() {
         System.out.println("DeleteUser");
-        String UserID = "";
         User_Controller instance = new User_Controller();
-        boolean expResult = false;
-        boolean result = instance.DeleteUser(UserID);
+        boolean expResult = true;
+        boolean result = instance.DeleteUser(testUser.UserID);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
+
 }
